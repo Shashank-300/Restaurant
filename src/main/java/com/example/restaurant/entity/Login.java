5e256354b44795072a6116d08697c9919f7e350e
Shashank-300 <vyasshashank300@gmail.com>
2024-11-20 00:29:51 +0530

    Added the Product Entity and deleted unwanted Login.java file

--- src/main/java/com/example/restaurant/entity/Login.java
+++
@@ -1,4 +0,0 @@
-package com.example.restaurant.entity;
-
-public class Login {
-}
---
+++ src/main/java/com/example/restaurant/entity/Product.java
@@ -0,0 +1,26 @@
+package com.example.restaurant.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @Column(name = "id", nullable = false)
+    private String id;
+    @Column(name = "price")
+    private double price;
+    @Column(name = "name")
+    private String name;
+}
